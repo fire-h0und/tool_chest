8cca8fde90e962bdbdf8093b54dbfa75fcb60780
fire-h0und <55053506+fire-h0und@users.noreply.github.com>
2022-05-22 21:18:03 +0200

    fixed a bug on asignment

--- kernel-upgrade.sh
+++ kernel-upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-V="0.3-rc1"
+V="0.4-rc1"
 echo -ne ".\n.\n.\n."
 echo "   BOOT UPDATER-"$V
 echo "========================================"
@@ -13,39 +13,51 @@ for X in /usr/sbin/grub-mkconfig /sbin/mkinitrd
   if [[ -x $X ]]
     then
     echo "found [$X]"
-    else
+  else
     echo "[$X] not found, make sure it is installed."
     echo "-Fatal error, exiting."
     exit 2
   fi
 done
-#default EFI path:
-efi_path="/boot/efi/EFI/Slackware"
-boot_path="/boot"
+#TODO:
+# add bootloader discrimination
+# add support for elilo and refind
+# add suppport for sbopkg
+
+if [[ -x /usr/sbin/sboinstall ]]
+  then
+  echo "-detected sbotools"
+  sbo_i=/usr/sbin/sboinstall
+  sbo_t()
+  {
+  True
+  }
+else
+  echo "-couldn't detect sbotools"
+fi
+
 rk=$(uname -r)
 echo "And we seem to be running on $rk kernel"
 echo "========================================"
 
-#TODO:
-# check for other bootloaders too
-
 #loop over the symlinks in /boot/
-for k in $boot_path/$vmlinuz*
+for k in /boot/vmlinuz*
 do
   #echo $k; file -b $k
-#  if file -b $k | grep -q "symbolic link"
-#    then echo "skipping the symlink (${k})"
-#  fi
+  if file -b $k | grep -q "symbolic link"
+    then echo "we have detected an symlink, skippin the symlink"
+  fi
   if file -b $k | grep -q "Linux kernel"
-    then echo "we have detected an kernel image:"
+    then echo "we have detected an kernel:"
     s=${k#*-}
     v=${s#*-}
     t=${s%-*}
     echo "it is an " $t "version" $v
     if [ "X"$t == "Xgeneric" ]
       then
-      echo "checking if the -generic needs any maintainence:"
-      for i in $boot_path/initrd-$v*
+      echo "does the -generic we're just detected need some maintainence:"
+      #check if initrd is needed!
+      for i in /boot/initrd-$v*
       do
         if ( file -b $i | grep -q "data" )
           then
@@ -56,7 +68,7 @@ do
               do
               if [ "X"$kv == "X"$v ] && [ "X"$kv != "X" ]
                 then
-                echo "initrd seems to be configured already."
+                echo "initrd seems to be configured already"
                 else
                 echo "editing /etc/mkinitrd.conf!"
                 #do it!
@@ -67,67 +79,68 @@ do
                 grep $newline /etc/mkinitrd.conf
               fi
             done
-            #we have appropriate mkintrd.conf now on
-            #proceed to make a initrd for the orphan -generic kernel
+            #a matching mkintrd.conf is present now on
+            #proceed to make an initrd for the still orphan -generic kernel
             /sbin/mkinitrd -F
-            new_initrd="${boot_path}/initrd-${v}.gz"
-            ####
-            cp -vu $new_initrd $efi_path
           fi
       done
     fi
-    new_kernel="${boot_path}/vmlinuz-${s}"
-    cp -vu $new_kernel $efi_path
-    echo "done processing ${new_kernel} kernel."
     #not an -generic, no further action needed
   fi
   #not a krenel, but a symlink, no action needed
 done
+#letting grup ovedo the config file now:
+echo "========================================"
+echo "Setting up boot loader:"
+echo "========================================"
 
-#TODO:
 #
-# check if grub, elilo or other supported bootloader is in use:
+#
 #
 
-#letting grup overdo the config file now:
-echo /usr/sbin/grub-mkconfig -o /boot/grub/grub.cfg
-
-# preparing for UEFI bootloader 
-# (by copying over the initrd and vmlinuz image)
-
-echo "Please edit the elilo conf fille accordingly"
+/usr/sbin/grub-mkconfig -o /boot/grub/grub.cfg
 
 
 # here on we check if there is an binary nvidia blob installed:
 echo "========================================"
 nv_i=$(which nvidia-installer)
-echo "found:"$nv_i
+echo "found:[${nv_i}]"
 echo "========================================"
 
 if [[ -x $nv_i ]]
   then
-   echo "NVidia blob is present!"
-   nv_v=`$nv_i -i | grep version | awk -F':' '{print $3}' | sed "s/)\.//"`
-
-   k_v=$(uname -r)
-
-   echo "Nvidia driver version:"${nv_v}" running kernel version:"${k_v}"; earlier kernel detected:"${v}
-   if [ "X"$k_v != "X"$v ] && [ "X"$v != "X" ]
-     then
-       echo "kernel mismatch, we default to the "${v}" kernel detected kernel in /boot/"
-       nv_d=`locate ${nv_v} | grep -E "NVIDIA|\.run"`
-       if [ "X"$nv_d != "X" ]
-       then
-         echo "found downloaded installer:"
-         echo "["${nv_d}"]"
-         sh $nv_d -ui=none -K -k ${v}
-       else
-         echo -e "can't locate the installer package, did you ran \n#updatedb\n recently?"
-       fi
-     else
-       echo "both kernels match or no kernel detected in /boot (?)"
-       $nv_i -K -k ${k_v}
-     fi
+  echo "NVidia blob is present!"
+  if $sbo_t
+    then
+    #try let sbotools handle the Nvidia blob
+    KERNEL=$v $sbo_i --reinstall -R -r nvidia-kernel
+    # --reinstall    rebuild a package even if present (our case here)
+    # -R perfrom no dependency resolution
+    # -r skip interaction with the user
   else
-   echo "no NVidia blob detected."
+    nv_v=`$nv_i -i | grep version | awk -F':' '{print $3}' | sed "s/)\.//"`
+
+    k_v=$(uname -r)
+
+    echo "Nvidia driver version:"${nv_v}" running kernel version:"${k_v}"; earlier kernel detected:"${v}
+    if [ "X"$k_v != "X"$v ] && [ "X"$v != "X" ]
+      then
+      echo "kernel mismatch, we default to the "${v}" kernel detected kernel in /boot/"
+      nv_d=`locate ${nv_v} | grep -E "NVIDIA|\.run"`
+      if [ "X"$nv_d != "X" ]
+        then
+        echo "found downloaded installer:"
+        echo "["${nv_d}"]"
+        sh $nv_d -ui=none -K -k ${v}
+      else
+        echo -e "can't locate the installer package, did you run \n#updatedb\n recently?"
+      fi
+      else
+        echo "both kernels match or no kernel detected in /boot (?)"
+        $nv_i -K -k ${k_v}
+      fi
+    fi
+else
+  echo "no NVidia blob detected."
 fi
+
