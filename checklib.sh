0609f2e475dfc5dc3a5aa0f9bbdd4c5750220a73
fire-h0und <55053506+fire-h0und@users.noreply.github.com>
2023-04-25 12:05:12 +0200

    Many times hounting missing *.so file
    This script simply seeks for a missing shared file

---
+++ checklib.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# version 1.0
+
+# kudos to Petri Kaukasoina
+# from linuxquestions.org
+
+echo "usage: $0 <SONAME>"
+echo
+echo 'objdump -p /usr/lib64/libsoup-2.4.so.1.11.2 | grep SONAME'
+echo 'SONAME               libsoup-2.4.so.1'
+
+[ $# -lt 1 ] && echo 'Usage, for example: ' $0 'libavcodec.so.59' && exit 1
+cd /var/adm/packages
+for pkg in *; do
+( cd /
+  while read line; do
+    [ "$line" == "FILE LIST:" ] && break
+  done
+  while read f; do
+    [ -x "$f" -a -f "$f" -a -r "$f" ] && grep -Eq "$1" "$f" && echo "$pkg": /"$f"
+  done 
+) < $pkg
+done
