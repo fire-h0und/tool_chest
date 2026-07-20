02a6bd517d772c1a77e98b616886cf2d6f9ee671
fire-h0und <55053506+fire-h0und@users.noreply.github.com>
2026-07-20 20:25:48 +0200

    Update sup.sh
    Recoded to be more efficient

--- sup.sh
+++ sup.sh
@@ -1,75 +1,25 @@
 #!/bin/bash
 
-tasks="nuc"
-# e - change editor
-# g - edit graylist
-# b - edit blacklist
+tasks="nukc"
 # n - install-new
 # u - upgrade-all
+# k - upgrade kernel
 # c - clean-system
 
-Editor=/usr/bin/mcedit
-# m - mcedit
-# v - vim
-# e - emacs
-# n - nano
-
 ### END DEFAULTS
-edcmd=(dialog --radiolist \"Tap_[esc]_twice_to_skip\" 13 40 4)
-if [[ -x /usr/bin/mcedit ]]; then
-  edcmd+=( m mcedit off)
-fi
-if [[ -x /usr/bin/vim ]]; then
-  edcmd+=( v vim off)
-fi
-if [[ -x /usr/bin/emacs ]]; then
-  edcmd+=( e emacs off)
-fi
-if [[ -x /usr/bin/nano ]]; then
-  edcmd+=( n nano off)
-fi
 
-dialog --backtitle "SUP - System Update Processor" --title "Operatios" --separator "" \
---item-help --nocancel --ok-label "Process" --no-tags --checklist "Tap [esc] twice to abort." 13 40 6 \
-e editor 0 "Change the editor used to modify lists and exit" \
-g greylist 0 "Change what is not selected" \
-b blacklist 0 "Change what is not listed" \
+dialog --backtitle "SUP - System Update Processor" --title "Operations" --separator "" \
+--item-help --nocancel --ok-label "Process" --no-tags --erase-on-exit \
+--checklist "Tap [esc] twice to abort." 13 40 6 \
 i install-new on "Select to install any newly added packages" \
 u upgrade-all on "Select to upgrade any installed packages" \
+k "upgrade kernel" on "Upgrade kernel" \
 c clean-system on "Select to remove any orphaned packages" 2> /tmp/task1.bin
-rep1=$(cat /tmp/task1.bin)
-echo $rep1
+rep1=$(cat /tmp/task.bin)
+#echo $rep1
 
 if slackpkg update; then
   case $rep1 in
-  *e*)
-    ${edcmd[@]} 2> /tmp/task2.bin
-    rep2=$(cat /tmp/task2.bin)
-    case $rep2 in
-    m)
-      echo "Selected mcedit"
-      Editor=/usr/bin/mcedit
-    ;;
-    v)
-      echo "Selected vim"
-      Editor=/usr/bin/vim
-    ;;
-    e)
-      echo "Selected emacs"
-      Editor=/usr/bin/emacs
-    ;;
-    n)
-      echo "Selected nano"
-      Editor=/usr/bin/nano
-    ;;
-  esac
-  ;;&
-  *g*)
-  $Editor /etc/slackpkg/greylist
-  ;;&
-  *b*)
-  $Editor /etc/slackpkg/blacklist
-  ;;&
   *i*)
     echo "Installing new packages:"
     slackpkg install-new
@@ -77,6 +27,13 @@ if slackpkg update; then
   *u*)
     echo "Upgrading all packages:"
     slackpkg upgrade-all
+  ;;&
+  *k*)
+    echo "Upgrading Kernel:"
+    klist=""
+    klist=$(slackpkg search kernel.* | grep "^\[.*\]" | awk -F: '{print $2;}') 
+    #echo "Kernel list: [${klist}]"
+    slackpkg upgrade $klist
   ;;&
   *c*)
     echo "Cleaning up orphans:"
@@ -87,4 +44,4 @@ fi
 echo "Checking for any remaining new configuration files:"
 slackpkg new-config
 echo "Cleanup:"
-rm /tmp/task[12].bin
+rm /tmp/task.bin
